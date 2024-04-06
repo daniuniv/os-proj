56e203aeab734f4229bb2fd50bcca521e927a9c4
daniuniv <daniel.blajovici@student.upt.ro>
2024-04-06 12:26:34 -0400

    output in a file instead of terminal

--- proj5.c
+++ proj5.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <fcntl.h>
-#include <string.h>
+#include <sys/types.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
 
-void function(const char *dirname) {
+void function(const char *dirname, int output_fd) {
     struct dirent *pDirent;
     DIR *pDir;
 
     // Open directory.
     pDir = opendir(dirname);
     if (pDir == NULL) {
-        printf("Cannot open directory '%s'\n", dirname);
+        char err_msg[100];
+        sprintf(err_msg, "Cannot open directory '%s'\n", dirname);
+        write(output_fd, err_msg, strlen(err_msg));
         return;
     }
 
     // Process each entry.
     while ((pDirent = readdir(pDir)) != NULL) {
         // Print entry info.
-        printf("--------------------\n");
-        printf("d_ino:[%ld]\n", pDirent->d_ino);
-        printf("d_off:[%ld]\n", pDirent->d_off);
-        printf("d_reclen:[%d]\n", pDirent->d_reclen);
-        printf("d_type:[%d]\n", pDirent->d_type);
-        printf("d_name:[%s]\n", pDirent->d_name);
-        printf("--------------------\n");
+        char entry_info[500];
+        sprintf(entry_info, "--------------------\nd_ino:[%ld]\nd_off:[%ld]\nd_reclen:[%d]\nd_type:[%d]\nd_name:[%s]\n--------------------\n",
+                pDirent->d_ino, pDirent->d_off, pDirent->d_reclen, pDirent->d_type, pDirent->d_name);
+        write(output_fd, entry_info, strlen(entry_info));
 
         // If entry is a directory, recursively call function.
         if (pDirent->d_type == DT_DIR) {
@@ -37,7 +36,7 @@ void function(const char *dirname) {
 
             char path[1024];
             snprintf(path, sizeof(path), "%s/%s", dirname, pDirent->d_name);
-            function(path);
+            function(path, output_fd);
         }
     }
 
@@ -52,8 +51,20 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    // Open file for writing
+    int output_fd = open("snapshot", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (output_fd == -1) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
     // Call function with specified directory.
-    function(argv[1]);
+    function(argv[1], output_fd);
+
+    // Close file
+    close(output_fd);
 
     return 0;
 }
+
+
